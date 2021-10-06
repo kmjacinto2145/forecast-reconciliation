ebe14ddaa43a7ce27c925277fb6186640207ff1a
kmjacinto2145 <59549173+kmjacinto2145@users.noreply.github.com>
2021-10-07 00:51:33 +1100

    Finished getting summing matrices for optimal combo

--- 06c - expanding window analysis (optimal combo).R
+++ 06c - expanding window analysis (optimal combo).R
@@ -130,8 +130,6 @@ names(jmd_female) = paste(names(jmd), "female", sep = "_")
 # ERROR CALCULATION
 ###############################################################################
 
-Next steps: obtain full summing matrix and revised forecasts
-
 ## PREFECTURE-SEX
 #Import revised forecasts for prefecture-sex (identical to base)
 R_prefec_sex = readRDS("exp_window/indep_forecasts_prefecsex.rds")
@@ -164,4 +162,169 @@ names(pt_combined_proj) = c(names(jmd_female %>%
                             names(jmd_male %>% 
                                       purrr::list_modify("Japan_male" = NULL))
                             )
+
+#Import projected pt for japan-sex
+pt_jpn_sex_female = readRDS("exp_window/pt_jpn_female_proj_sex.rds")
+names(pt_jpn_sex_female) = names(jmd_female %>% 
+                                     purrr::list_modify("Japan_female" = NULL))
+pt_jpn_sex_male = readRDS("exp_window/pt_jpn_male_proj_sex.rds")
+names(pt_jpn_sex_male) = names(jmd_male %>% 
+                                   purrr::list_modify("Japan_male" = NULL))
+
+#Import projected pt for japan-total
+pt_jpn_total_female = readRDS("exp_window/pt_jpn_female_proj.rds")
+pt_jpn_total_male = readRDS("exp_window/pt_jpn_male_proj.rds")
+pt_jpn_total = c(pt_jpn_total_female, pt_jpn_total_male)
+names(pt_jpn_total) = names(pt_combined_proj)
+
+#Convert column names into integers
+pt_jpn_sex_female = mclapply(pt_jpn_sex_female, function(data){
+    colnames(data) = 1:55
+    return(data)
+})
+
+pt_jpn_sex_male = mclapply(pt_jpn_sex_male, function(data){
+    colnames(data) = 1:55
+    return(data)
+})
+
+pt_jpn_total = mclapply(pt_jpn_total, function(data){
+    colnames(data) = 1:55
+    return(data)
+})
+
+#Convert pt lists into list of matrices - rows: h-series, columns: prefecsex
+St_jpn_total = list()
+
+for (age in 0:100) {
+    print(age)
+    tmp_matrix = matrix(nrow = 0, ncol = PREFEC_COUNT * 2)
+    
+    for (series in 1:55) {
+        tmp_vec = sapply(pt_jpn_total, function(m) {
+            return(m[as.character(age),series])
+        })
+        tmp_matrix = rbind(tmp_matrix, tmp_vec)
+    }
+    St_jpn_total[[as.character(age)]] = tmp_matrix
+}
+
+#Row 2 of St: japan-female
+zero_vec = rep(x = 0, times = PREFEC_COUNT)
+
+St_jpn_female = list()
+
+for (age in 0:100) {
+    print(age)
+    tmp_matrix = matrix(nrow = 0, ncol = PREFEC_COUNT * 2)
+    
+    for (series in 1:55) {
+        tmp_vec = sapply(pt_jpn_sex_female, function(m) {
+            return(m[as.character(age),series])
+        })
+        
+        tmp_vec = c(tmp_vec, zero_vec)
+
+        tmp_matrix = rbind(tmp_matrix, c(tmp_vec))
+    }
+    colnames(tmp_matrix) = names(pt_jpn_total)
+    
+    St_jpn_female[[as.character(age)]] = tmp_matrix
+}
+
+#Row 3 of St: Japan-male
+St_jpn_male = list()
+
+for (age in 0:100) {
+    print(age)
+    tmp_matrix = matrix(nrow = 0, ncol = PREFEC_COUNT * 2)
+    
+    for (series in 1:55) {
+        tmp_vec = sapply(pt_jpn_sex_male, function(m) {
+            return(m[as.character(age),series])
+        })
+        
+        tmp_vec = c(zero_vec, tmp_vec)
+        
+        tmp_matrix = rbind(tmp_matrix, c(tmp_vec))
+    }
+    colnames(tmp_matrix) = names(pt_jpn_total)
+    
+    St_jpn_male[[as.character(age)]] = tmp_matrix
+}
+
+#Reorder columns
+colorder = to_vec(for (i in 1:PREFEC_COUNT) c(i, i + PREFEC_COUNT))
+St_jpn_total = St_jpn_total %>% mclapply(function(data) {
+    return(data[,colorder])
+})
+
+St_jpn_female = St_jpn_female %>% mclapply(function(data) {
+    return(data[,colorder])
+})
+
+St_jpn_male = St_jpn_male %>% mclapply(function(data) {
+    return(data[,colorder])
+})
+
+#Add rows for prefecture totals
+St_prefec_total = list()
+
+for (age in 0:100) {
+    print(age)
+    tmp_list = list()
+    
+    for (series in 1:55) {
+        tmp_vec = sapply(pt_combined_proj, function(m) {
+            return(m[as.character(age),series])
+        })
+        prefec_vec = c()
+        for (prefec in 1:PREFEC_COUNT) {
+            prefec_vec = append(prefec_vec, c(tmp_vec[prefec], 
+                                              tmp_vec[prefec + PREFEC_COUNT],
+                                              rep(0, times = PREFEC_COUNT * 2)))
+        }
+        prefec_matrix = matrix(prefec_vec, ncol = PREFEC_COUNT * 2, byrow = T)
+        prefec_matrix = prefec_matrix[1:47,]
+        tmp_list[[as.character(series)]] = prefec_matrix
+    }
+    St_prefec_total[[as.character(age)]] = tmp_list
+}
+
+St_prefecsex =  diag(PREFEC_COUNT * 2)
+
+#Get indices of rows with each h-value
+hx = list()
+
+h = 1
+lim = 10
+for (i in 1:55) {
+    hx[[as.character(h)]] = append(hx[[as.character(h)]], i)
+    
+    if (h == lim) {
+        h = 1
+        lim = lim - 1
+    } else {
+        h = h + 1
+    }
+}
+
+St_list = list()
+for (series in 1:55) {
+    print(series)
+    St_age = list()
+    for (age in 0:100) {
+        St = matrix(nrow = 0, ncol = PREFEC_COUNT * 2)
+        
+        St_1 = St_jpn_total[[as.character(age)]][series,]
+        St_2 = St_jpn_female[[as.character(age)]][series,]
+        St_3 = St_jpn_male[[as.character(age)]][series,]
+        St_4 = St_prefec_total[[as.character(age)]][[as.character(series)]]
+        St_5 = St_prefecsex
+        
+        St = rbind(St, St_1, St_2, St_3, St_4, St_5)
+        St_age[[as.character(age)]] = St
+    }
+    St_list[[as.character(series)]] = St_age
+}
 
